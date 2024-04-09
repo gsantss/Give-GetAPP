a953ec96bcce474c35a6828de4234268c8dbe97d
WandreySantos <wandreymiceoni16@gmail.com>
2024-04-09 16:06:58 -0300

    asdasdasd

--- lib/Publicar.dart
+++ lib/Publicar.dart
@@ -8,87 +8,111 @@ class Publicar extends StatefulWidget {
 class _PublicarItemScreenState extends State<Publicar> {
   String nome = '';
   String descricao = '';
-  String categoriaSelecionada = 'Categoria';
+  String comunidadeSelecionada = 'Comunidades';
 
-  final List<String> categorias = [
-    'Categoria',
-    'Eletrônicos',
-    'Moda',
-    'Casa',
-    'Esportes',
+  final List<String> comunidades = [
+    'Comunidades',
+    'PUC-MG',
+    'PUC-RJ',
+    'UFMG',
+    'USP',
+    'UFOP',
     'Outros',
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  // Adicione aqui a ação desejada quando a caixa vazia for clicada
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 100,
-                  
-                  child: Image.asset(
-                    "assets/Imagens.png",
-                    fit: BoxFit.cover,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 9, 12, 14), // Cor primária do tema (barra de título, botões)
+        scaffoldBackgroundColor: Colors.white, // Cor de fundo do Scaffold
+        // Outras propriedades de estilo aqui
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF11224D), // Define a cor de fundo da AppBar
+          title: Text(
+            'Publicar',
+            style: TextStyle(color: Colors.white), // Define a cor do texto da AppBar
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+                20.0, 20.0, 20.0, 80.0), // Padding adicionado na parte inferior
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    // Adicione aqui a ação desejada quando a caixa vazia for clicada
+                  },
+                  child: AspectRatio(
+                    aspectRatio:
+                        5 / 5, // Proporção da imagem (ajuste conforme necessário)
+                    child: Image.asset(
+                      "assets/Imagens.png", // Caminho da imagem
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-
-
-              SizedBox(height: 50),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Nome do Item'),
-                onChanged: (value) {
-                  setState(() {
-                    nome = value;
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Descrição'),
-                maxLines: null,
-                onChanged: (value) {
-                  setState(() {
-                    descricao = value;
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              DropdownButtonFormField(
-                value: categoriaSelecionada,
-                items: categorias.map((categoria) {
-                  return DropdownMenuItem(
-                    value: categoria,
-                    child: Text(categoria),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    categoriaSelecionada = value.toString();
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Lógica para publicar o item
-                  print('Nome: $nome');
-                  print('Descrição: $descricao');
-                  print('Categoria: $categoriaSelecionada');
-                },
-                child: Text('Publicar'),
-              ),
-            ],
+                SizedBox(height: 50),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome do Item'),
+                  onChanged: (value) {
+                    setState(() {
+                      nome = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  maxLines: null,
+                  onChanged: (value) {
+                    setState(() {
+                      descricao = value;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                DropdownButtonFormField(
+                  value: comunidadeSelecionada,
+                  hint: Text(
+                      'Selecione uma comunidade'), // Adiciona uma dica ao campo
+                  items: comunidades.map((comunidade) {
+                    return DropdownMenuItem(
+                      value: comunidade,
+                      child: Text(comunidade),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      comunidadeSelecionada = value.toString();
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xFF11224D),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Lógica para publicar o item
+                    print('Nome: $nome');
+                    print('Descrição: $descricao');
+                    print('Comunidade: $comunidadeSelecionada');
+                  },
+                  child: Text(
+                    'Publicar',
+                    style: TextStyle(color: Colors.white), // Cor do texto do botão
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -97,8 +121,5 @@ class _PublicarItemScreenState extends State<Publicar> {
 }
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Publicar(),
-  ));
+  runApp(Publicar());
 }
